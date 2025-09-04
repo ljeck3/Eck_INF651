3bdf32ee41340de4fcd7ef08586778a088402280
lj-1003 <levijustin24@gmail.com>
2025-09-03 23:04:59 -0500

    Add Assignment 2

---
+++ Assignment/assignment2/main.js
@@ -0,0 +1,63 @@
+// Variables and Data Types
+
+let firstLastName = 'Marquis Nordyke';
+let age = 21;
+let isStudent = true;
+
+console.log(firstLastName);
+console.log(age);
+console.log(isStudent);
+console.log("-------------------------------");
+
+// Basic Arithmetic Operations
+
+let firstNumber = 5;
+let secondNumber = 15;
+
+console.log(firstNumber + secondNumber);
+console.log(secondNumber - firstNumber);
+console.log(firstNumber * secondNumber);
+console.log(secondNumber / firstNumber);
+console.log("-------------------------------");
+
+// Working with Strings
+
+let sentence = "I am learning JavaScript.";
+
+console.log(sentence.length);
+console.log(sentence.charAt(0));
+console.log(sentence.charAt(sentence.length - 1));
+console.log("-------------------------------");
+
+//Math Object
+let negativeNumber = -100;
+
+console.log(Math.sqrt(negativeNumber));
+console.log( Math.pow(negativeNumber, 2));
+console.log(Math.abs(negativeNumber))
+console.log("-------------------------------");
+
+// Boolena Logic and Comparison Operators
+let newFirstNumber = 67;
+let newSecondNumber = 41;
+
+console.log(newFirstNumber > newSecondNumber);
+console.log(newFirstNumber < newSecondNumber);
+console.log(newFirstNumber == newSecondNumber);
+console.log("-------------------------------");
+
+// Logical Operators
+let hasSiblings = false;
+let hasPets = true;
+
+console.log(hasSiblings && hasPets);
+console.log(hasSiblings || hasPets);
+console.log(!hasSiblings);
+console.log(!hasPets);
+console.log("-------------------------------");
+
+// Using Template Literals
+let firstName = "Hank";
+let lastName = "Hill";
+console.log(`Hello, my name is ${firstName} ${lastName}. This is my wife Peggy and my son Bobby.`);
+
